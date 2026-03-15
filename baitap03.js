3520b9a372389ebfb879221b7298a6e137afda67
yen21-git <tranyen4002@gmail.com>
2026-03-16 08:34:34 +0900

    Add files via upload

---
+++ baitap03.js
@@ -0,0 +1,4 @@
+let width = Number(prompt('Nhập vào chiều dài: '));
+let height = Number(prompt('Nhập vào chiều rộng: '));
+
+console.log('Diện tích là: ' + width * height);
---
+++ baitap04.js
@@ -0,0 +1,4 @@
+let firstNumber = Number(prompt('Nhập vào số thứ nhất: '));
+let secondNumber = Number(prompt('Nhập vào số thứ hai: '));
+
+console.log('Tổng hai số là: ' + (firstNumber + secondNumber));
